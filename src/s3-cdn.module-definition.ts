570d04aee95df715981863394ca29eac1df7e2ae
bit0r1n <me@bitor.in>
2025-04-28 17:48:22 +0300

    module: isGlobal value

--- src/s3-cdn.module-definition.ts
+++ src/s3-cdn.module-definition.ts
@@ -2,4 +2,14 @@ import { ConfigurableModuleBuilder } from '@nestjs/common'
 import { S3CdnOptions } from './interfaces'
 
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
-  new ConfigurableModuleBuilder<S3CdnOptions>().build()
+  new ConfigurableModuleBuilder<S3CdnOptions>()
+    .setExtras(
+      {
+        isGlobal: false,
+      },
+      (definition, extras) => ({
+        ...definition,
+        global: extras.isGlobal,
+      }),
+    )
+    .build()
